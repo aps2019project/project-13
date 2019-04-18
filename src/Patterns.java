23f2ece7fbf39b7d39a8489422730452de762305
Amirmahdi Namjoo <amirm137878@gmail.com>
2019-04-19 01:26:57 +0430

    Added pattern class for input patterns.  14/4/2019 - Amirmahdi Namjoo

---
+++ src/Patterns.java
@@ -0,0 +1,55 @@
+import java.util.regex.Pattern;
+
+public class Patterns {
+    //This class maybe will be merged in input system. But as of now, it is just a Place for Patterns;
+    public static Pattern[] accountPatterns = new Pattern[]{
+            Pattern.compile("^back\\s+$"),
+            Pattern.compile("^create account (\\S+)\\s*$"),
+            Pattern.compile("^login (\\S+)\\s*$"),
+            Pattern.compile("^show leaderboard\\s*$"),
+            Pattern.compile("^save\\s*$"),
+            Pattern.compile("^logout\\s*$"),
+            Pattern.compile("^help\\s*$"),
+    };
+    public static Pattern[] mainMenuPatterns = new Pattern[]{
+            Pattern.compile("^back\\s+$"),
+            Pattern.compile("^Enter Collection\\s*$"),
+            Pattern.compile("^Enter Shop\\s*$"),
+            Pattern.compile("^Enter Battle\\s*$"),
+            Pattern.compile("^Enter Exit\\s*$"),
+            Pattern.compile("^Enter Help\\s*$"),
+    };
+    public static Pattern[] collectionPatterns = new Pattern[]{
+            Pattern.compile("^back\\s+$"),
+            Pattern.compile("^exit\\s*$"),
+            Pattern.compile("^show\\s*$"),
+            Pattern.compile("^search (\\S+)\\s*$"),
+            Pattern.compile("^save\\s*$"),
+            Pattern.compile("^create deck (\\S+)\\s*$"),
+            Pattern.compile("^delete deck (\\S+)\\s*$"),
+            Pattern.compile("^add (\\S+) to deck (\\S+)\\s*$"),
+            Pattern.compile("^remove (\\S+) from deck (\\S+)\\s*$"),
+            Pattern.compile("^validate deck (\\S+)\\s*$"),
+            Pattern.compile("^select deck (\\S+)\\s*$"),
+            Pattern.compile("^show all decks\\s*$"),
+            Pattern.compile("^show deck (\\S+)\\s*$"),
+            Pattern.compile("^help$")
+    };
+    public static  Pattern[] shopPatterns = new Pattern[]{
+            Pattern.compile("^back\\s*$"),
+            Pattern.compile("^exit\\s*$"),
+            Pattern.compile("^show collection\\s*$"),
+            Pattern.compile("^search (\\S+)\\s*$"),
+            Pattern.compile("^search collection (\\S+)\\s*$"),
+            Pattern.compile("^buy (\\S+)\\s*$"),
+            Pattern.compile("^sell (\\S+)\\s*$"),
+            Pattern.compile("^show (\\S+)\\s*$"),
+            Pattern.compile("^help (\\S+)\\s*$"),
+    };
+    public static  Pattern[] battlePatterns = new Pattern[]{
+            Pattern.compile("^Game info\\s*$"),
+            Pattern.compile("^Show my minions\\s*$"),
+            Pattern.compile("^Show opponent minions\\s*$"),
+            Pattern.compile("^Show card info (\\S+)\\s*$")
+    };
+}
