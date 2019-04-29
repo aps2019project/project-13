ad73136dc5bfb9c6669b9b9ce6cd52ac55302f6c
rezaaminimajd <45604304+rezaaminimajd@users.noreply.github.com>
2019-04-29 22:43:37 +0430

    show_help Change
    Signed-off-by: rezaaminimajd <45604304+rezaaminimajd@users.noreply.github.com>

--- src/View/Show.java
+++ src/View/Show.java
@@ -6,6 +6,17 @@ public class Show {
     private static final String GET_PASSWORD = "PassWord [ password > 5 letter ] : ";
     private static final String UNRELIBALE_PASSWORD = "password is unreliable! , input secure password";
     private static final String YOUR_PASSWORD = "PassWord : ";
+    private static final String INCORRECT_PASSWORD = "incorrect password! , try again!";
+    private static final String COLLECTABLE_HELP = "Show info\nUse [location x,y]";
+    private static final String GRAVEYARD_HELP = "exit\nShow info\nShow Cards";
+    private static final String SHOP_HELP = "Show menu\nshow collection\nsearch [card name]\nsearch collection [name]\nbuy [name]\nsell [name]\nshow";
+    private static final String BATTLE_HELP = "Game info\nShow my minions\nShow opponent minions\n" +
+            "Show card info [cardID]\nSelect [card id]\nInsert [card name] in (x,y)\nEnd turn\nShow collectables\n" +
+            "Show Next Card\nShow hand\nEnter graveyard\nEnd Game\nMove to (x,y)\nAttack [[opponent card id]\nUse special power (x,y)";
+    private static final String COLLECTION_HELP = "Show menu\nshow\nsearch [card name] \nsave \ncreate deck [deck name] \ndelete deck [deck name]" +
+            "\nadd [card id] to deck [deck name]\nremove [card id] from deck [deck name] \nvalidate deck [deck name]\n" +
+            "select deck [deck name] \nshow all decks \nshow deck [deck name]";
+    public static final String MAIN_MENU_HELP = "Show menu\nEnter Collection\nEnter Shop\nEnter Battle\nEnter Exit";
 
     private Show() {
 
@@ -18,31 +29,75 @@ public class Show {
     public void showHelp(KindOfOrder kindOfOrder) {
         switch (kindOfOrder) {
             case COLLECTABLE:
+                showCollectabletHelp();
+                break;
             case GRAVEYARD:
-            case CARD:
+                showGraveYardHelp();
+                break;
             case SHOP:
+                showShopHelp();
+                break;
             case BATTLE:
+                showBattleHelp();
+                break;
             case ACCOUNT:
                 showAccountHelp();
+                break;
             case MAIN_MENU:
+                showMainMenuHelp();
+                break;
             case COLLECTION:
+                showColectionHelp();
+                break;
         }
     }
-    private void showAccountHelp(){
+
+    private void showAccountHelp() {
         System.out.println(HELP_SHOW);
     }
-    public void getPassword(){
+
+    public void getPassword() {
         System.out.println(GET_PASSWORD);
     }
-    public void unreliablePassWord(){
+
+    public void unreliablePassWord() {
         System.out.println(UNRELIBALE_PASSWORD);
     }
-    public void createdAccount(String username){
-        System.out.println("new account "+username+" is created");
+
+    public void createdAccount(String username) {
+        System.out.println("new account " + username + " is created");
     }
-    public void getYourPasWord(){ System.out.println(YOUR_PASSWORD);}
-    public void incorrectPassWord(){
-        System.out.println(ConstantMessages.INVALID_PASSWORD);
+
+    public void getYourPasWord() {
+        System.out.println(YOUR_PASSWORD);
+    }
+
+    public void incorrectPassWord() {
+        System.out.println(INCORRECT_PASSWORD);
+    }
+
+    private void showCollectabletHelp() {
+        System.out.println(COLLECTABLE_HELP);
+    }
+
+    private void showGraveYardHelp() {
+        System.out.println(GRAVEYARD_HELP);
+    }
+
+    private void showShopHelp() {
+        System.out.println(SHOP_HELP);
+    }
+
+    private void showBattleHelp() {
+        System.out.println(BATTLE_HELP);
+    }
+
+    private void showColectionHelp() {
+        System.out.println(COLLECTION_HELP);
+    }
+
+    private void showMainMenuHelp() {
+        System.out.println(MAIN_MENU_HELP);
     }
 
 }
