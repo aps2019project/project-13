045647945bf81d4b607760c2e085060b05690a65
alireza <alirezashateri7@gmail.com>
2019-05-02 23:18:26 +0430

    update method in buffs

--- src/Model/BuffClasses/ABuff.java
+++ src/Model/BuffClasses/ABuff.java
@@ -5,14 +5,20 @@ import Model.Account;
 public abstract class ABuff {
     int duration;
     Account account;
+    PositiveNegative positiveNegative;
 
-    public ABuff(Account account, int duration ) {
+    public ABuff(Account account, int duration , PositiveNegative positiveNegative) {
         this.account = account;
         this.duration = duration;
+        this.positiveNegative = positiveNegative;
     }
 
     public abstract <T> void affect(T t);
 
+
+    public PositiveNegative getPositiveNegative() {
+        return positiveNegative;
+    }
     public abstract <T> void update(T t);
 
     public Account getAccount() {
